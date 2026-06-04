8962e28b390ec43f227b12dcf1f90d66be46759b
SalahHassan202 <salahhassan66190@gmail.com>
2026-06-05 01:36:21 +0300

    feat: added footer widget file

---
+++ lib/features/home/widgets/footer_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../core/constants/app_colors.dart';
+import '../../core/constants/app_strings.dart';
+import '../../core/theme/app_dimensions.dart';
+
+class FooterWidget extends StatelessWidget {
+  const FooterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: AppDimensions.paddingMD,
+        vertical: AppDimensions.paddingLG,
+      ),
+      child: Column(
+        children: [
+          const Divider(color: AppColors.divider),
+          const SizedBox(height: AppDimensions.spaceMD),
+          Text(
+            '${AppStrings.appName} • ${AppStrings.appTagline} ♡',
+            style: GoogleFonts.lato(
+              fontSize: 12,
+              color: AppColors.textHint,
+              letterSpacing: 0.5,
+            ),
+          ),
+          const SizedBox(height: AppDimensions.spaceSM),
+          Text(
+            '© 2024 All rights reserved',
+            style: GoogleFonts.lato(
+              fontSize: 11,
+              color: AppColors.textHint,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
