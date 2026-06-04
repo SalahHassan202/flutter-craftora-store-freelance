f5ce711df160c0cffa902c31b6f826ccf6d780db
SalahHassan202 <salahhassan66190@gmail.com>
2026-06-05 01:26:10 +0300

    feat: build search bar with filter

---
+++ lib/features/home/widgets/search_bar_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/app_colors.dart';
+import '../../core/constants/app_strings.dart';
+import '../../core/theme/app_dimensions.dart';
+import '../../core/theme/app_text_theme.dart';
+
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            height: AppDimensions.searchBarHeight,
+            decoration: BoxDecoration(
+              color: AppColors.searchBg,
+              borderRadius: BorderRadius.circular(AppDimensions.radiusMD),
+            ),
+            child: Row(
+              children: [
+                const SizedBox(width: AppDimensions.paddingMD),
+                const Icon(
+                  Icons.search_rounded,
+                  color: AppColors.textHint,
+                  size: 20,
+                ),
+                const SizedBox(width: AppDimensions.spaceSM),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: AppStrings.searchHint,
+                      hintStyle: AppTextTheme.searchHint,
+                      border: InputBorder.none,
+                      isDense: true,
+                    ),
+                    style: AppTextTheme.searchHint.copyWith(
+                      color: AppColors.textPrimary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(width: AppDimensions.spaceSM),
+        Container(
+          height: AppDimensions.searchBarHeight,
+          width: AppDimensions.searchBarHeight,
+          decoration: BoxDecoration(
+            color: AppColors.searchBg,
+            borderRadius: BorderRadius.circular(AppDimensions.radiusMD),
+          ),
+          child: const Icon(
+            Icons.tune_rounded,
+            color: AppColors.textSecondary,
+            size: 20,
+          ),
+        ),
+      ],
+    );
+  }
+}
