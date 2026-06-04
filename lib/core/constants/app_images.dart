989cc64b9ee7b32bf69063c4e4a752359b097505
SalahHassan202 <salahhassan66190@gmail.com>
2026-06-05 01:15:40 +0300

    feat: added image urls

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,45 @@
+class AppImages {
+  AppImages._();
+
+  static const String bannerCandle =
+      'https://images.unsplash.com/photo-1602928321679-560bb453f190?w=800&auto=format&fit=crop';
+
+  static const String categoryCandles =
+      'https://images.unsplash.com/photo-1603006905003-be475563bc59?w=400&auto=format&fit=crop';
+
+  static const String categoryAccessories =
+      'https://images.unsplash.com/photo-1535632066927-ab7c9ab60908?w=400&auto=format&fit=crop';
+
+  static const String categoryCrochet =
+      'https://images.unsplash.com/photo-1604881988758-f76ad2f7aac1?w=400&auto=format&fit=crop';
+
+  static const String categoryJewelry =
+      'https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?w=400&auto=format&fit=crop';
+
+  static const String categoryPottery =
+      'https://images.unsplash.com/photo-1565193566173-7a0ee3dbe261?w=400&auto=format&fit=crop';
+
+  static const String productBubbleCandle =
+      'https://images.unsplash.com/photo-1608181831718-c9fba5b11ca6?w=400&auto=format&fit=crop';
+
+  static const String productCrochetBag =
+      'https://images.unsplash.com/photo-1590874103328-eac38a683ce7?w=400&auto=format&fit=crop';
+
+  static const String productBeadedBracelet =
+      'https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?w=400&auto=format&fit=crop';
+
+  static const String productFragranceCandle =
+      'https://images.unsplash.com/photo-1602928321679-560bb453f190?w=800&auto=format&fit=crop';
+
+  static const String productFragranceCandle2 =
+      'https://images.unsplash.com/photo-1603006905003-be475563bc59?w=800&auto=format&fit=crop';
+
+  static const String productFragranceCandle3 =
+      'https://images.unsplash.com/photo-1612540139004-a9270e0e9d98?w=800&auto=format&fit=crop';
+
+  static const String productMacrame =
+      'https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=400&auto=format&fit=crop';
+
+  static const String productCeramicVase =
+      'https://images.unsplash.com/photo-1565193566173-7a0ee3dbe261?w=400&auto=format&fit=crop';
+}
