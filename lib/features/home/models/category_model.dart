323fb9b819a15a636f5a9f37566ec5bd123088a0
SalahHassan202 <salahhassan66190@gmail.com>
2026-06-05 01:22:55 +0300

    feat: define category data model

---
+++ lib/features/home/models/category_model.dart
@@ -0,0 +1,13 @@
+class CategoryModel {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final int itemCount;
+
+  const CategoryModel({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.itemCount,
+  });
+}
