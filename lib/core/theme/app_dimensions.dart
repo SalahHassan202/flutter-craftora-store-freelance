7ed3794a6547a5560b8a2d551f09445b41a92599
SalahHassan202 <salahhassan66190@gmail.com>
2026-06-05 01:17:21 +0300

    feat: define spacing radius and sizes

---
+++ lib/core/theme/app_dimensions.dart
@@ -0,0 +1,39 @@
+class AppDimensions {
+  AppDimensions._();
+
+  static const double radiusXS = 6.0;
+  static const double radiusSM = 10.0;
+  static const double radiusMD = 14.0;
+  static const double radiusLG = 20.0;
+  static const double radiusXL = 28.0;
+  static const double radiusCircle = 100.0;
+
+  static const double paddingXS = 4.0;
+  static const double paddingSM = 8.0;
+  static const double paddingMD = 16.0;
+  static const double paddingLG = 24.0;
+  static const double paddingXL = 32.0;
+
+  static const double spaceXS = 4.0;
+  static const double spaceSM = 8.0;
+  static const double spaceMD = 12.0;
+  static const double spaceLG = 16.0;
+  static const double spaceXL = 24.0;
+  static const double space2XL = 32.0;
+
+  static const double iconSM = 18.0;
+  static const double iconMD = 22.0;
+  static const double iconLG = 26.0;
+
+  static const double categoryCardSize = 100.0;
+  static const double productCardWidth = 160.0;
+  static const double productCardHeight = 200.0;
+  static const double productCardImageHeight = 130.0;
+
+  static const double bannerHeight = 180.0;
+  static const double navBarHeight = 64.0;
+  static const double searchBarHeight = 48.0;
+
+  static const double appBarLogoHeight = 32.0;
+  static const double elevationCard = 2.0;
+}
